a4a1b7bf3fe8e24d4e230b0a21fe5a3698f9d9b8
Asad Abdullah <97662778+AbdullahAsad96@users.noreply.github.com>
2025-01-07 11:36:59 +0000

    Updating var let and const in same file

---
+++ BasicSyntax/1-OPerators.js
@@ -0,0 +1,17 @@
+// console.log("control majnu");
+// let age =32;
+// console.log(age+'ads');
+   /// Null
+// const NullNum=null
+// console.log(NaN==NullNum);
+
+// discussion of let var and const
+if(true){
+   let num1=20; //can't be intailized more then once in the same block  
+   var num2=30;    //can be intialized more then once in same block/ Scope 
+   const num3="1001"; //just like a constants you can't update their values
+}
+console.log(num2); //executed // because var can be accessed outside the scope.
+console.log(num1, num3) // let and const can't be accessed outside the scope of function or outside of a block e.g block=if{....}
+
+
