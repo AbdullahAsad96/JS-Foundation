c3914dc5f174740c14ab063b479ca9742d804aa3
Asad Abdullah <97662778+AbdullahAsad96@users.noreply.github.com>
2025-03-23 19:22:47 +0000

    Math objects added

---
+++ BasicSyntax/objects.js
@@ -0,0 +1,8 @@
+const Pi= Math.PI
+// console.log(Pi)
+// console.log(Math.round(Pi))
+let gravity =9.87
+// console.log(Math.round(gravity)) 10
+// console.log(Math.random()*10) 
+// console.log(Math.ceil(gravity)) 10
+// console.log(Math.floor(gravity)) 9
