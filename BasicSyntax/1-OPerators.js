1fa757b101533d11767421161d658ef61dfb7cd3
Asad Abdullah <97662778+AbdullahAsad96@users.noreply.github.com>
2025-03-29 06:46:03 +0000

    boolean operator

--- BasicSyntax/1-OPerators.js
+++ BasicSyntax/1-OPerators.js
@@ -182,4 +182,6 @@ let sentance="You cannot end a sentence with because because because is a conjun
 // console.log('10'==10)
 // random num btw 50 to 500
 let randomNum=Math.floor(Math.random()*100-50+1)+50;
-console.log(randomNum);
+// console.log(randomNum);
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+console.log(sentence)
---
+++ BasicSyntax/binaryOperators.js
@@ -0,0 +1,4 @@
+// let fName, lName, age, country, city, isMarried= 'Asad', 'Abdullah', 23, 'Pakistan', 'Narowal', Flase;
+let fName='Asad';
+let lname='Abdullah';
+
