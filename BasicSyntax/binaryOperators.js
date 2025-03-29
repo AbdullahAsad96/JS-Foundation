815c8e2675eb3a9d3b7b339494d2d4dbc36c44c2
Asad Abdullah <97662778+AbdullahAsad96@users.noreply.github.com>
2025-03-29 06:53:11 +0000

    commit

--- BasicSyntax/binaryOperators.js
+++ BasicSyntax/binaryOperators.js
@@ -1,4 +1,7 @@
 // let fName, lName, age, country, city, isMarried= 'Asad', 'Abdullah', 23, 'Pakistan', 'Narowal', Flase;
 let fName='Asad';
 let lname='Abdullah';
-
+let age=24;
+let country='Pakistan';
+isMarried= false;
+console.log(fName, lname, )
